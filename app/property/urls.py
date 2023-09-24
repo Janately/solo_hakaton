7a748a2c716a875f1fd6efc14f655412823f8a03
Janately <kimerkin3@gmail.com>
2023-09-25 04:47:30 +0600

    Hakaton_project proba#1

---
+++ app/property/urls.py
@@ -0,0 +1,10 @@
+from rest_framework.routers import DefaultRouter
+from .views import PropertyViewSet
+
+routers = DefaultRouter()
+
+routers.register('', PropertyViewSet, basename='allproperty')
+
+urlpatterns = [
+
+              ] + routers.urls
